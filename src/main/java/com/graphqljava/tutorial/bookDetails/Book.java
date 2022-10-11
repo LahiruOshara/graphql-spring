417e252d5f8eea0cf77e758e00af1897d0f23958
LahiruOshara <oshara.16@cse.mrt.ac.lk>
2022-10-11 09:23:49 +0530

    refactored code in Book.java

--- src/main/java/com/graphqljava/tutorial/bookDetails/Book.java
+++ src/main/java/com/graphqljava/tutorial/bookDetails/Book.java
@@ -6,14 +6,10 @@ import java.util.List;
 public class Book {
 
     private String id;
-    private String name;
-    private int pageCount;
     private String authorId;
 
     public Book(String id, String name, int pageCount, String authorId) {
         this.id = id;
-        this.name = name;
-        this.pageCount = pageCount;
         this.authorId = authorId;
     }
 
