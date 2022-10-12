c85d3cf50b5b6ffeeb19c64141dcba39c7c53238
LahiruOshara <oshara.16@cse.mrt.ac.lk>
2022-10-12 22:35:00 +0530

    refactored code

--- src/main/java/com/graphqljava/tutorial/bookDetails/Book.java
+++ src/main/java/com/graphqljava/tutorial/bookDetails/Book.java
@@ -7,21 +7,17 @@ public class Book {
 
     private final String id;
     private final String authorId;
-    private final String name;
-    private final int pageCount;
 
-    public Book(String id, String name, int pageCount, String authorId) {
+    public Book(String id, String authorId) {
         this.id = id;
         this.authorId = authorId;
-        this.name = name;
-        this.pageCount = pageCount;
     }
 
 //    hard coded list
-    private static List<Book> books = Arrays.asList(
-            new Book("book-1", "Harry Potter and the Philosopher's Stone", 223, "author-1"),
-            new Book("book-2", "Moby Dick", 635, "author-2"),
-            new Book("book-3", "Interview with the vampire", 371, "author-3")
+    private static final List<Book> books = Arrays.asList(
+            new Book("book-1", "author-1"),
+            new Book("book-2", "author-2"),
+            new Book("book-3", "author-3")
     );
 
     public static Book getById(String id) {
